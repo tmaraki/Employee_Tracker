8284f38ef8301fb020bbdaac85b7613cc395bc3b
tmaraki <toshiemaraki@gmail.com>
2024-02-25 11:52:57 -0500

    updated server.js file adding function start code to start application and connected to mysql db

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const mysql = require("mysql2");
 
 const PORT = process.env.PORT || 3001;
 
-// Connect to database
+// connect to db
 const db = mysql.createConnection(
   {
     host: 'localhost',
@@ -13,3 +13,72 @@ const db = mysql.createConnection(
   },
   console.log(`Connected to the movies_db database.`)
 );
+
+// function to start application
+function start() {
+    inquirer
+        .prompt({
+            type: "list",
+            name: "action",
+            message: "What would you like to do?",
+            choices: [
+                "View all departments",
+                "View all roles",
+                "View all employees",
+                "Add a department",
+                "Add a role",
+                "Add an employee",
+                "Add a Manager",
+                "Update an employee role",
+                "View Employees by Manager",
+                "View Employees by Department",
+                "Delete Departments | Roles | Employees",
+                "View the total utilized budget of a department",
+                "Exit",
+            ],
+        })
+        .then((answer) => {
+            switch (answer.action) {
+                case "View all departments":
+                    viewAllDepartments();
+                    break;
+                case "View all roles":
+                    viewAllRoles();
+                    break;
+                case "View all employees":
+                    viewAllEmployees();
+                    break;
+                case "Add a department":
+                    addDepartment();
+                    break;
+                case "Add a role":
+                    addRole();
+                    break;
+                case "Add an employee":
+                    addEmployee();
+                    break;
+                case "Add a Manager":
+                    addManager();
+                    break;
+                case "Update an employee role":
+                    updateEmployeeRole();
+                    break;
+                case "View Employees by Manager":
+                    viewEmployeesByManager();
+                    break;
+                case "View Employees by Department":
+                    viewEmployeesByDepartment();
+                    break;
+                case "Delete Departments | Roles | Employees":
+                    deleteDepartmentsRolesEmployees();
+                    break;
+                case "View the total utilized budget of a department":
+                    viewTotalUtilizedBudgetOfDepartment();
+                    break;
+                case "Exit":
+                    connection.end();
+                    console.log("Goodbye!");
+                    break;
+            }
+        });
+}
